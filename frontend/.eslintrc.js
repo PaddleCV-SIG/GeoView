0e6102691de3dcb50dfa7dffaed20deff9541214
yibaikuai <you@example.com2276001806@qq.com>
2022-09-22 16:31:18 +0800

    eslint规范

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -3,12 +3,6 @@ module.exports = {
       'plugin:vue/vue3-recommended'
     ],
     rules: {
-      'vue/multi-word-component-names': 'off',
-        'vue/no-parsing-error': [
-            2, {
-                'x-invalid-end-tag': false,
-            },
-        ]
-
+      'vue/multi-word-component-names': 'off'
     }
   }
