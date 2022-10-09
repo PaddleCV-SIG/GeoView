d09a6bd09afd16e95bba465a8f293d2d90751720
Lu Lidong <2421235683@qq.com>
2022-10-09 11:36:48 +0800

    修复变化检测工作不正常的bug

--- backend/applications/interface/change_detection.py
+++ backend/applications/interface/change_detection.py
@@ -1,3 +1,4 @@
+import os
 import os.path as osp
 
 import numpy as np
@@ -10,19 +11,25 @@ from applications.common.path_global import generate_url
 
 
 def execute(model_path, data_path, out_dir, names, window_size=256, stride=128):
+    image_list = [(osp.join(data_path, name["first"]), osp.join(data_path,
+                                                                name["second"]))
+                  for name in names]
     temps = list()  # 存储查看链接
     temps1 = list()  # 存储生成的图片名
     predictor = pdrs.deploy.Predictor(model_path, use_gpu=True)
-    predictor.slider_predict(
-        data_path,
-        save_dir=out_dir,
-        transforms=None,
-        block_size=window_size,  #注意block_size的值不能等于overlap的值
-        overlap=stride)
+    for image in image_list:
+        predictor.slider_predict(
+            image,
+            save_dir=out_dir,
+            transforms=None,
+            block_size=window_size,  #注意block_size的值不能等于overlap的值
+            overlap=stride)
     for name in names:
-        img = decode_image(osp.join(out_dir, name))
+        raw_name = name["first"].split(".")[0] + ".tif"
+        img = decode_image(osp.join(out_dir, raw_name))
         save_img = np.where(img == 0, img, 255)
-        imsave(osp.join(out_dir, name), save_img)
-        temps.append(generate_url + name)
-        temps1.append(name)
+        imsave(osp.join(out_dir, name["first"]), save_img)
+        temps.append(generate_url + name["first"])
+        temps1.append(name["first"])
+        os.remove(osp.join(out_dir, raw_name))
     return temps, temps1
