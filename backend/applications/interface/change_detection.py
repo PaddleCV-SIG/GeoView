3fca7b17a0a810867d7308f31ea22321d3887b84
Lu Lidong <2421235683@qq.com>
2022-10-10 13:28:03 +0800

    按review进行修改

--- backend/applications/interface/change_detection.py
+++ backend/applications/interface/change_detection.py
@@ -23,9 +23,9 @@ def execute(model_path, data_path, out_dir, names, window_size=256, stride=128):
             save_dir=out_dir,
             transforms=None,
             block_size=window_size,  #注意block_size的值不能等于overlap的值
-            overlap=stride)
+            overlap=window_size - stride)
     for name in names:
-        raw_name = name["first"].split(".")[0] + ".tif"
+        raw_name = os.path.splitext(name["first"])[0] + ".tif"
         img = decode_image(osp.join(out_dir, raw_name))
         save_img = np.where(img == 0, img, 255)
         imsave(osp.join(out_dir, name["first"]), save_img)
