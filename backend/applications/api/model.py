436c902dbeec27bca7ffd08b2c6e5f9a7fe87510
Lu Lidong <2421235683@qq.com>
2022-10-13 11:03:19 +0800

    Feature/custom port sync (#12)
    * Feature/custom port (#11)
    
    * 自定义权重
    
    * 支持自定义端口号
    
    * 自定义监听IP
    
    * 修复当model/{model_type}目录不存在时后端接口抛出异常的问题。

--- backend/applications/api/model.py
+++ backend/applications/api/model.py
@@ -20,17 +20,18 @@ def get_model_list(model_type):
     if model_type not in types_list:
         return fail_api("模型类型不正确")
     model_list = []
-    for dirname in os.listdir("model/{}".format(model_type)):
-        try:
-            model_info = get_model_info("model/{}/{}".format(model_type,
-                                                             dirname))
-            if model_info["_Attributes"]["model_type"] == types_list[
-                    model_type]:
-                model_list.append({
-                    "model_path": "model/{}/{}".format(model_type, dirname),
-                    "model_type": model_info["_Attributes"]["model_type"],
-                    "model_name": model_info["Model"]
-                })
-        except:
-            pass
+    if os.path.exists("model/{}".format(model_type)):
+        for dirname in os.listdir("model/{}".format(model_type)):
+            try:
+                model_info = get_model_info("model/{}/{}".format(model_type,
+                                                                 dirname))
+                if model_info["_Attributes"]["model_type"] == types_list[
+                        model_type]:
+                    model_list.append({
+                        "model_path": "model/{}/{}".format(model_type, dirname),
+                        "model_type": model_info["_Attributes"]["model_type"],
+                        "model_name": model_info["Model"]
+                    })
+            except:
+                pass
     return success_api(data=model_list)
