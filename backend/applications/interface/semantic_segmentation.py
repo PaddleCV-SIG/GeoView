9140f45e482a005634646d427e2939e344e6cddf
Lu Lidong <2421235683@qq.com>
2022-10-30 11:49:35 +0800

    lut颜色随机生成

--- backend/applications/interface/semantic_segmentation.py
+++ backend/applications/interface/semantic_segmentation.py
@@ -1,20 +1,19 @@
 import os.path as osp
+from collections import Counter
 
 import cv2
 import numpy as np
 import paddlers as pdrs
+from paddlers.tasks.utils.visualize import get_color_map_list
 from skimage.io import imsave
 
 from applications.common.path_global import md5_name, generate_url
 
 
-def get_lut():
+def get_lut(classes_num):
     lut = np.zeros((256, 3), dtype=np.uint8)
-    lut[0] = [255, 0, 0]  # 红
-    lut[1] = [30, 255, 142]  # 浅绿
-    lut[2] = [60, 0, 255]  # 蓝
-    lut[3] = [255, 222, 0]  # 橙黄
-    lut[4] = [255, 0, 255]  # 粉
+    lut[0:classes_num] = get_color_map_list(classes_num + 1)[1:]  # +1是为了去掉黑色
+    print(lut)
     return lut
 
 
@@ -23,10 +22,9 @@ def execute(model_path, data_path, out_dir, test_names):
     predictor = pdrs.deploy.Predictor(model_path, use_gpu=True)
     pred = predictor.predict(image_list)
     ims = [i['label_map'] for i in pred]
-    lut = get_lut()
     temps = list()
     for idx, im in zip(range(len(image_list)), ims):
-        im = lut[im]
+        im = get_lut(np.max(im) + 1)[im]
         new_name = md5_name(test_names[idx])
         imsave(osp.join(out_dir, new_name), im)
         temps.append(generate_url + new_name)
