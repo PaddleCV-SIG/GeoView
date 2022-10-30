1773b5df8884cacbd3952d44f7608df84b68f980
Lu Lidong <2421235683@qq.com>
2022-10-30 11:52:15 +0800

    lut颜色随机生成

--- backend/applications/interface/semantic_segmentation.py
+++ backend/applications/interface/semantic_segmentation.py
@@ -11,9 +11,8 @@ from applications.common.path_global import md5_name, generate_url
 
 
 def get_lut(classes_num):
-    lut = np.zeros((256, 3), dtype=np.uint8)
-    lut[0:classes_num] = get_color_map_list(classes_num + 1)[1:]  # +1是为了去掉黑色
-    print(lut)
+    lut = np.zeros((classes_num, 3), dtype=np.uint8)
+    lut[0:classes_num] = get_color_map_list(classes_num)
     return lut
 
 
