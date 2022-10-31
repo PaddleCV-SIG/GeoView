2d357ae11c0b6a9bf7551b8a1fef680798b8227d
Lu Lidong <2421235683@qq.com>
2022-10-31 15:48:29 +0800

    指定最大类别数

--- backend/applications/interface/semantic_segmentation.py
+++ backend/applications/interface/semantic_segmentation.py
@@ -16,8 +16,9 @@ def execute(model_path, data_path, out_dir, test_names):
     pred = predictor.predict(image_list)
     ims = [i['label_map'] for i in pred]
     temps = list()
+    lut = np.array(get_color_map_list(256))
     for idx, im in zip(range(len(image_list)), ims):
-        im = np.array(get_color_map_list(np.max(im) + 1))[im]
+        im = lut[im]
         new_name = md5_name(test_names[idx])
         imsave(osp.join(out_dir, new_name), im)
         temps.append(generate_url + new_name)
