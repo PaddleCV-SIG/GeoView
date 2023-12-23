46fb691b4e34cbe5a040f8ae065fbe69c123792d
卢利栋 <2421235683@qq.com>
2023-12-23 22:55:14 +0800

    分离前后端的配置文件，便于实现docker部署

--- backend/app.py
+++ backend/app.py
@@ -29,13 +29,7 @@ migrate = Migrate(app, db)
 
 if __name__ == '__main__':
     import yaml
-    with open('../config.yaml') as file:
+    with open('./config.yaml') as file:
         config = yaml.load(file.read(), Loader=yaml.FullLoader)
     debug_mode = bool(config.get("debug", False))
-    with open("../frontend/.env", 'w') as file:
-        file.write(
-            "VUE_APP_BACKEND_PORT = {}\nVUE_APP_BACKEND_IP = {}\nVUE_APP_BAIDU_MAP_ACCESS_KEY = {}".
-            format(config["port"]["backend"], config["host"]["backend"]
-                   if config["host"]["backend"] != "0.0.0.0" else "127.0.0.1",
-                   config["baidu_map"]["access_key"]))
     app.run(host=config["host"]["backend"], port=config["port"]["backend"])
